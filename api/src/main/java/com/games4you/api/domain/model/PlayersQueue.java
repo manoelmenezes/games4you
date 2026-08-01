d98c73706ff28e87f8f9083a62385d051ea61551
Manoel Menezes <manoel.menezes.jr@gmail.com>
2026-08-01 20:16:26 +0000

    Players Queue

---
+++ api/src/main/java/com/games4you/api/domain/model/PlayersQueue.java
@@ -0,0 +1,65 @@
+package com.games4you.api.domain.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class PlayersQueue {
+
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    private Long playerId;
+
+    public PlayersQueue() {
+
+    }
+
+    public PlayersQueue(Long playersId) {
+        this.playerId = playersId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(Long playerId) {
+        this.playerId = playerId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PlayersQueue)) {
+            return false;
+        }
+        PlayersQueue other = (PlayersQueue) o;
+        return Objects.equals(this.id, other.id) && Objects.equals(this.playerId, other.playerId);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.playerId);
+    }
+
+    @Override
+    public String toString() {
+        return "PlayersQueue{id=" + this.id + ", playerId=" + this.playerId + "}";
+    }
+    
+}
