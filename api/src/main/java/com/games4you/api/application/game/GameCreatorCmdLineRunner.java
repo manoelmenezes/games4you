879b4d735560167a58713068cdf21c2212ebca7c
Manoel Menezes <manoel.menezes.jr@gmail.com>
2026-08-02 04:24:09 +0000

    Use just 1 worker.

--- api/src/main/java/com/games4you/api/application/game/GameCreatorCmdLineRunner.java
+++ api/src/main/java/com/games4you/api/application/game/GameCreatorCmdLineRunner.java
@@ -10,7 +10,7 @@ import java.util.concurrent.Executors;
 @Configuration
 public class GameCreatorCmdLineRunner {
 
-    private static final int POOL_SIZE = 3;
+    private static final int POOL_SIZE = 1;
 
      @Bean
      CommandLineRunner startGameCreatorWorkers(GameCreatorWorker worker) {
