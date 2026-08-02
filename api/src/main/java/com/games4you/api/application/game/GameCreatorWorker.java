4ef9414cc915609b2a650dc0e9c0127f3944c182
Manoel Menezes <manoel.menezes.jr@gmail.com>
2026-08-02 04:11:56 +0000

    Fix usage of Transactional in worker.

--- api/src/main/java/com/games4you/api/application/game/GameCreatorWorker.java
+++ api/src/main/java/com/games4you/api/application/game/GameCreatorWorker.java
@@ -18,15 +18,12 @@ public class GameCreatorWorker implements Runnable {
 
     private static final Logger log = LoggerFactory.getLogger(GameCreatorWorker.class);
 
-    private static final String NEW_GAME_BOARD_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     private static final int SLEEP_TIME_MS = 1000;
 
-    private final PlayersQueueRepository playersQueueRepository;
-    private final GameRepository gameRepository;
+    private final GameService gameService;
 
-    public GameCreatorWorker(PlayersQueueRepository playersQueueRepository, GameRepository gameRepository) {
-        this.playersQueueRepository = playersQueueRepository;
-        this.gameRepository = gameRepository;
+    public GameCreatorWorker(GameService gameService) {
+        this.gameService = gameService;    
     }
 
     @Override
@@ -37,7 +34,7 @@ public class GameCreatorWorker implements Runnable {
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 // Your continuous business logic goes here
-                createGame();
+                gameService.createGame();
 
                 // Prevent 100% CPU utilization by adding a controlled pause
                 Thread.sleep(SLEEP_TIME_MS);
@@ -49,40 +46,6 @@ public class GameCreatorWorker implements Runnable {
                 log.error("Error occurred during task execution", e);
             }
         }
-    }
-
-    @Transactional
-    private void createGame() {
-        log.info("Processing ongoing background work...");
-
-        PlayersQueue pq1 = getPlayer();
-        PlayersQueue pq2 = getPlayer();
-
-        Game game = new Game(pq1.getPlayerId(), pq2.getPlayerId(), NEW_GAME_BOARD_FEN);
-        gameRepository.save(game);
-        
-        playersQueueRepository.delete(pq1);
-        playersQueueRepository.delete(pq2);
-    }
-
-    private PlayersQueue getPlayer() {
-        Optional<PlayersQueue> pq = Optional.empty();
-        do {
-            try {
-                pq = playersQueueRepository.findForUpdate();
-                Thread.sleep(SLEEP_TIME_MS);
-            } catch (InterruptedException e) {
-                log.info("Worker thread interrupted. Shutting down gracefully...");
-                // Restore interrupted status to cleanly exit the loop
-                Thread.currentThread().interrupt(); 
-            }
-        } while (!Thread.currentThread().isInterrupted() && pq.isEmpty());
-
-        if (pq.isEmpty()) {
-            throw new RuntimeException("Player not found");
-        }
-
-        return pq.get();        
     }
     
 }
--- api/src/main/java/com/games4you/api/application/game/GameService.java
+++ api/src/main/java/com/games4you/api/application/game/GameService.java
@@ -2,6 +2,8 @@ package com.games4you.api.application.game;
 
 import java.util.Optional;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import com.games4you.api.domain.model.game.Game;
@@ -9,9 +11,14 @@ import com.games4you.api.domain.model.game.GameRepository;
 import com.games4you.api.domain.model.game.PlayersQueue;
 import com.games4you.api.domain.model.game.PlayersQueueRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class GameService {
 
+    private static final Logger log = LoggerFactory.getLogger(GameService.class);
+
+    private static final String NEW_GAME_BOARD_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     private static final int MAX_RETRIES = 10;
     private static final int SLEEP_TIME_MS = 1000;
     
@@ -46,6 +53,40 @@ public class GameService {
         } while (!Thread.currentThread().isInterrupted() && game.isEmpty() && retry < MAX_RETRIES);
 
         return game;
+    }
+
+    @Transactional
+    public void createGame() {
+        log.info("Creating game...");
+
+        PlayersQueue pq1 = getPlayer();
+        PlayersQueue pq2 = getPlayer();
+
+        Game game = new Game(pq1.getPlayerId(), pq2.getPlayerId(), NEW_GAME_BOARD_FEN);
+        gameRepository.save(game);
+        
+        playersQueueRepository.delete(pq1);
+        playersQueueRepository.delete(pq2);
+    }
+
+    private PlayersQueue getPlayer() {
+        Optional<PlayersQueue> pq = Optional.empty();
+        do {
+            try {
+                pq = playersQueueRepository.findForUpdate();
+                Thread.sleep(SLEEP_TIME_MS);
+            } catch (InterruptedException e) {
+                log.info("Worker thread interrupted. Shutting down gracefully...");
+                // Restore interrupted status to cleanly exit the loop
+                Thread.currentThread().interrupt(); 
+            }
+        } while (!Thread.currentThread().isInterrupted() && pq.isEmpty());
+
+        if (pq.isEmpty()) {
+            throw new RuntimeException("Player not found");
+        }
+
+        return pq.get();        
     }
 
 }
